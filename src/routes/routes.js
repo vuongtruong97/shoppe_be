e3701237bfbd54dbaac6791a2916046c32ccc663
Vuong Truong <truongquocvuongnb@gmail.com>
2022-06-07 01:34:29 +0700

    index

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const userRouter = require('./user.route')
 
 const configRouter = (app) => {
+    app.use('*', (req, res) => res.send('shopee_be_by_vuong_truong'))
     app.use('/users', userRouter)
 }
 
