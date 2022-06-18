a8d29b4b2a88010b42033c1f13cef0784e146b28
Vuong Truong <94800760+vuongtruong97@users.noreply.github.com>
2022-06-18 14:46:30 +0700

    Update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user.model')
+const User = require('../models/User.model')
 const bcrypt = require('bcrypt')
 const { SECRET_KEY, EXPERT_KEY } = process.env
 const jwt = require('jsonwebtoken')
