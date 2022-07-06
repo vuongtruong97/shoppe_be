5fe407ea97c7ddc37ee6a4588c3672fe70d4ae36
Vuong Truong <truongquocvuongnb@gmail.com>
2022-07-06 11:44:26 +0700

    test file system cache

--- src/controllers/ImageController.js
+++ src/controllers/ImageController.js
@@ -1,22 +1,34 @@
 const Image = require('../models/Image.model')
-const {
-    Api404Error,
-    Api409Error,
-    Api429Error,
-    Api422Error,
-} = require('../lib/custom-error-handler/apiError')
+const { Api404Error } = require('../lib/custom-error-handler/apiError')
+const Cache = require('file-system-cache').default
+
+const cache = Cache({
+    basePath: './.cache', // Optional. Path where cache files are stored (default).
+    ns: 'my-namespace', // Optional. A grouping namespace for items.
+})
 
 module.exports = {
     async getImage(req, res, next) {
         try {
             const { id } = req.params
 
-            const image = await Image.findById(id)
-            if (!image) {
-                throw new Api404Error()
+            const existCache = await cache.get(id)
+
+            if (existCache) {
+                const img = JSON.parse(existCache)
+                res.set('Content-Type', img.content_type)
+                res.send(new Buffer(img.data.data, 'binary'))
+            } else {
+                const image = await Image.findById(id)
+                if (!image) {
+                    throw new Api404Error()
+                }
+
+                await cache.set(id, JSON.stringify(image))
+
+                res.set('Content-Type', image.content_type)
+                res.send(image.data)
             }
-            res.set('Content-Type', image.content_type)
-            res.send(image.data)
         } catch (error) {
             next(error)
         }
