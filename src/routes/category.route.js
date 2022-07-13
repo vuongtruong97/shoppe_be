37ff29946231757ed34d69d5da159bb211357c43
vuong truong <vuongtruong@Vuong-Pc.localdomain>
2022-07-14 02:00:13 +0700

    conflict 2

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const CategoryController = require('../controllers/CategoryController')
 const { categoryImage } = require('../lib/multer')
@@ -18,25 +17,3 @@ categoryRouter.get('/list-category', CategoryController.getListCategory)
 categoryRouter.get('/list-prod/:slug', CategoryController.getProdOfCate)
 
 module.exports = categoryRouter
-=======
-const express = require('express')
-const CategoryController = require('../controllers/CategoryController')
-const { categoryImage } = require('../lib/multer')
-const { validateCate } = require('../middlewares/validators/category-validator')
-
-const categoryRouter = express.Router()
-
-categoryRouter.post(
-    '/add-category',
-    categoryImage.single('image'),
-    validateCate,
-    CategoryController.addCategory
-)
-categoryRouter.patch('/update-category/:slug', CategoryController.updateCategory)
-categoryRouter.delete('/delete-category/:slug', CategoryController.deleteCategory)
-categoryRouter.get('/image/:slug', CategoryController.getCateImageById)
-categoryRouter.post('/list-category', CategoryController.getListCategory)
-categoryRouter.get('/list-prod/:slug', CategoryController.getProdOfCate)
-
-module.exports = categoryRouter
->>>>>>> a30e69258abb7728fc1e981be9420856fb279489
